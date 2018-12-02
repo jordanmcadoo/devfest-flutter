c4838f2cd2fa3c9475f6760d5817686f62538f31
Jordan McAdoo <jordan.mcadoo@stitchfix.com>
2018-12-01 18:44:01 -0600

    setup tflite model stuff

--- witch_app/lib/not_hotdog.dart
+++ witch_app/lib/not_hotdog.dart
@@ -1,20 +1,116 @@
-import 'package:image_picker/image_picker.dart';
+import 'dart:async';
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'package:tflite/tflite.dart';
+import 'package:image_picker/image_picker.dart';
+
 import 'custom_theme.dart' as Theme;
 
-class NotHotdog extends StatelessWidget {
+//class NotHotdog extends StatelessWidget {
+//  Future getImage() async {
+//    var image = await ImagePicker.pickImage(source: ImageSource.gallery);
+//  }
+//  Widget build(BuildContext context) {
+//    return Scaffold(
+//        appBar: AppBar(
+//          title: Text('Not Hotdog'),
+//        ),
+//        backgroundColor: Theme.CustomColors.lightPink[500],
+//        body: Center(
+//          child: FloatingActionButton(onPressed: getImage)
+//        )
+//    );
+//  }
+//}
+
+class NotHotdog extends StatefulWidget {
+  @override
+  _NotHotdogState createState() => new _NotHotdogState();
+}
+
+class _NotHotdogState extends State<NotHotdog> {
+  File _image;
+  List _recognitions;
+
   Future getImage() async {
     var image = await ImagePicker.pickImage(source: ImageSource.gallery);
+    recognizeImage(image);
+    setState(() {
+      _image = image;
+    });
   }
+
+  @override
+  void initState() {
+    super.initState();
+    loadModel();
+  }
+
+  Future loadModel() async {
+    try {
+      String res = await Tflite.loadModel(
+        model: "assets/graph.tflite",
+        labels: "assets/labels.txt",
+      );
+      print(res);
+    } on PlatformException {
+      print('Failed to load model.');
+    }
+  }
+
+  Future recognizeImage(File image) async {
+    var recognitions = await Tflite.runModelOnImage(
+      path: image.path,
+      numResults: 6,
+      threshold: 0.05,
+    );
+    print(recognitions);
+    setState(() {
+      _recognitions = recognitions;
+    });
+    // await Tflite.close();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return MaterialApp(
+      home: Scaffold(
         appBar: AppBar(
-          title: Text('Not Hotdog'),
+          title: const Text('Not Hotdog'),
         ),
-        backgroundColor: Theme.CustomColors.lightPink[500],
-        body: Center(
-          child: FloatingActionButton(onPressed: getImage)
-        )
+        body: Stack(
+          children: <Widget>[
+            Center(
+              child: _image == null
+                  ? Text('No image selected.')
+                  : Image.file(_image),
+            ),
+            Center(
+              child: Column(
+                children: _recognitions != null
+                    ? _recognitions.map((res) {
+                  return Text(
+                    "${res["index"]} - ${res["label"]}: ${res["confidence"].toString()}",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20.0,
+                      background: Paint()..color = Colors.white,
+                    ),
+                  );
+                }).toList()
+                    : [],
+              ),
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: getImage,
+          tooltip: 'Pick Image',
+          child: Icon(Icons.add_a_photo),
+        ),
+      ),
     );
   }
 }
